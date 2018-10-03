33caf3f3c38d08c16c11d7b7ee21d83a506ec060
Facens Exchange <markinho9_7@hotmail.com>
2018-10-03 02:01:30 +0000

    login facebook[5]

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -146,6 +146,6 @@
             debugger;
             console.log('Successful login for: ' + response.name);
             document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!';
-        });
+        }, {scope : 'email, user_likes'});
     }
 </script>
