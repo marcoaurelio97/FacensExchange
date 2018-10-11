1e84a299efbf34ef128b96623339f45b43bf8e13
Marco <markinho9_7@hotmail.com>
2018-10-11 16:02:01 -0300

    correção itens

--- application/models/Model_itens.php
+++ application/models/Model_itens.php
@@ -18,7 +18,7 @@ class Model_itens extends CI_Model {
     public function getItems($idProfile,$status = FALSE){
         $this->db->where('item_idprofile',$idProfile);        
         $this->db->join('itens_pictures', 'item_id = itempic_iditem', 'left');
-        $this->db->where('item_views','ASC');
+        $this->db->order_by('item_views','ASC');
         if($status || $status === '0'){
             $this->db->where('item_status',$status);
         }
